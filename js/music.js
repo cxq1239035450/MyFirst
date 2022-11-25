d1b1782ef1163bba9b2d2f831488075cc9c5af8e
chenxiaoqiang <chenxiaoqiang@dcx.com>
2022-11-25 17:52:17 +0800

    增加音乐播放器按钮

--- js/music.js
+++ js/music.js
@@ -2,12 +2,17 @@ const music = document.getElementById('music');
 const musicImg = document.getElementById('musicImg')
 const bar = document.getElementById('bar')
 const musictext = document.getElementById('musictext');
+const musicSwitch = document.getElementById('musicSwitch')
+const musicBox = document.getElementById('musicBox')
+const contentBox = document.getElementById('contentBox')
 var text = [
     '每个人都很孤独，在人的一生中，遇到爱、遇到性都不稀罕，稀罕得到是遇到了解。',
     '不要做一边渴望，又一边拒绝的人，打开自己有些困难，但是没关系，我们可以慢慢来。',
     '时间或许像灯光，日照褪尽了才能看见，彼此靠近但是没有关联，一盏熄灭了，另一盏刚好亮了。',
     '当你下定决定做一件事，那就去尽力做，给自己一个期限，不用告诉所有人，也不要犹豫，直到你真的尽力为止。'
 ]
+// 是否展开
+let MUSIC_OPEN = false
 // 播放状态
 let MUSIC_STATIC = false
 // 音乐长度
@@ -26,12 +31,28 @@ music.oncanplay = ()=>{
         musictext.innerText = text[randomNum]
     }, 0);
 }
+let closeMusic = null
+const close = ()=>{
+    closeMusic = setTimeout(() => {
+       contentBox.style.display="none"
+       musicSwitch.style.display = 'block'
+       musicBox.style.width = ''
+       musicBox.style.right = '0'
+    }, 5000);
+}
+musicSwitch.onclick = ()=>{
+    contentBox.style.display="block"
+    musicSwitch.style.display = 'none'
+    musicBox.style.width = '160px'
+    musicBox.style.right = '10px'
+        close()
+}
+
 let num = 0
 //
 function rollText(item){
 
     const p = item /10 -num
-    console.log(p,num);
     const textWidth = (musictext.offsetWidth + 180) *p + num*100
     const time = Math.abs(textWidth / 60)
     const textLeft = Math.abs(musictext.style.left.slice(0,musictext.style.left.length-2))
@@ -53,6 +74,10 @@ function rollText(item){
 // }
 
 function playMusic(){
+    if(closeMusic){
+        clearTimeout(closeMusic)
+        close()
+    }
     if(MUSIC_STATIC){
         musicImg.src = './icon/play.png'
         music.pause()
